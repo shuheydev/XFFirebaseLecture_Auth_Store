0c8552114474ddb84d739e40e3d49915f123f433
shuheydev <shuhey.dev@gmail.com>
2020-08-04 22:20:44 +0900

    temporary saved

---
+++ XFFirebaseLecture_Auth_Store/XFFirebaseLecture_Auth_Store.Android/Dependencies/Auth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using XFFirebaseLecture_Auth_Store.ViewModels.Helpers;
+
+namespace XFFirebaseLecture_Auth_Store.Droid.Dependencies
+{
+    public class Auth : IAuth
+    {
+        public Task<bool> AuthenticateUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAuthenticated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+
+                var profileUpdates = new Firebase.Auth.UserProfileChangeRequest.Builder();
+                profileUpdates.SetDisplayName(name);
+                var build = profileUpdates.Build();
+
+                var user = Firebase.Auth.FirebaseAuth.Instance.CurrentUser;
+                await user.UpdateProfileAsync(build);
+
+                return true;
+            }
+            catch(FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ XFFirebaseLecture_Auth_Store/XFFirebaseLecture_Auth_Store/ViewModels/Helpers/AuthHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFFirebaseLecture_Auth_Store.ViewModels.Helpers
+{
+    public interface IAuth
+    {
+        Task<bool> RegisterUser(string name, string email, string password);
+        Task<bool> AuthenticateUser(string email, string password);
+        bool IsAuthenticated();
+        string GetCurrentUserId();
+    }
+
+    /// <summary>
+    /// DIの必要性の説明のために作ったのかな?
+    /// </summary>
+    public class Auth
+    {
+        private static IAuth auth;
+        public static async Task< bool> RegisterUser(string name, string email, string password)
+        {
+            return await auth.RegisterUser(name, email, password);
+        }
+
+        public static async Task<bool> AuthenticateaUser( string email, string password)
+        {
+            return await auth.AuthenticateUser(email, password);
+        }
+
+        public static bool IsAuthenticated()
+        {
+            return auth.IsAuthenticated();
+        }
+
+        public static string GetCurrentUserId()
+        {
+            return auth.GetCurrentUserId();
+        }
+    }
+}
