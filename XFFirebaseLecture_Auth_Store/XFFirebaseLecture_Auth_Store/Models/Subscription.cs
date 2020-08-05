25711f631b2f67ceb4b73ef7be55ad89b3aa88d7
shuheydev <shuhey.dev@gmail.com>
2020-08-05 15:00:02 +0900

    implemented firestore class which provide operation of firestore.

---
+++ XFFirebaseLecture_Auth_Store/XFFirebaseLecture_Auth_Store/Models/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFFirebaseLecture_Auth_Store.Models
+{
+    public class Subscription
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime SubscribedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Subscription()
+        {
+
+        }
+    }
+}
---
+++ XFFirebaseLecture_Auth_Store/XFFirebaseLecture_Auth_Store/ViewModels/Helpers/DatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XFFirebaseLecture_Auth_Store.Models;
+
+namespace XFFirebaseLecture_Auth_Store.ViewModels.Helpers
+{
+    public interface   IFirestore
+    {
+        Task<bool> InsertSubscription(Subscription subscription);
+        Task<bool> DeleteSubscription(Subscription subscription);
+        Task<bool> UpdateSubscription(Subscription subscription);
+        Task<IList<Subscription>> ReadSubscriptions();
+
+    }
+
+    public class DatabaseHelper
+    {
+        private static IFirestore firestore;
+
+        public Task<bool> DeleteSubscription(Subscription subscription)
+        {
+            return firestore.DeleteSubscription(subscription);
+        }
+
+        public bool InsertSubscription(Subscription subscription)
+        {
+            return firestore.InsertSubscription(subscription);
+        }
+
+        public Task<IList<Subscription>> ReadSubscriptions()
+        {
+            return firestore.ReadSubscriptions();
+        }
+
+        public Task<bool> UpdateSubscription(Subscription subscription)
+        {
+            return firestore.UpdateSubscription(subscription);
+        }
+    }
+}
